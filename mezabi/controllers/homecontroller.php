b66732b7d642f890de602b9e67f122c9745d99ec
Mathieu Palosse <78204424+mpalosse@users.noreply.github.com>
2024-10-23 15:29:10 +0200

    Update homecontroller.php
    Ajout de try catch dans homecontroller

--- mezabi/controllers/homecontroller.php
+++ mezabi/controllers/homecontroller.php
@@ -6,14 +6,18 @@ use yasmf\View;
 class HomeController {
 
     public function index($pdo) {
-        $sql = "select code_categorie, designation 
-            from a_categories
-            order by code_categorie";
-        $searchStmt = $pdo->prepare($sql);
-        $searchStmt->execute();
-        $view = new View("/views/all_categories");
-        $view->setVar('searchStmt',$searchStmt);
-        return $view;
+        try {
+            $sql = "select code_categorie, designation 
+                from a_categories
+                order by code_categorie";
+            $searchStmt = $pdo->prepare($sql);
+            $searchStmt->execute();
+            $view = new View("/views/all_categories");
+            $view->setVar('searchStmt',$searchStmt);
+            return $view;
+        } catch(PDOException $exception) {
+            throw new PDOException($exception->getMessage(), (int)$exception->getCode());
+        }
     }
 
 }
