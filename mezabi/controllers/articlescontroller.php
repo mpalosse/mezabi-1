91af53228a3cafe4755dacac69afef2cb3518238
Mathieu Palosse <78204424+mpalosse@users.noreply.github.com>
2024-10-23 15:29:40 +0200

    Update articlescontroller.php
    Ajout de try catch dans articlescontroller

--- mezabi/controllers/articlescontroller.php
+++ mezabi/controllers/articlescontroller.php
@@ -7,18 +7,22 @@ use yasmf\View;
 class ArticlesController {
 
     public function index($pdo) {
-        $codeCategorie = HttpHelper::getParam("code_categorie");
-        $designationCategorie = HttpHelper::getParam("categorie");
-        $sql = "select id_article, code_article, designation 
-            from articles
-            where categorie = ?
-            order by code_article";
-        $searchStmt = $pdo->prepare($sql);
-        $searchStmt->execute([$codeCategorie]);
-        $view = new View("/views/all_articles");
-        $view->setVar('searchStmt',$searchStmt);
-        $view->setVar('categorie', $designationCategorie);
-        return $view;
+        try {
+            $codeCategorie = HttpHelper::getParam("code_categorie");
+            $designationCategorie = HttpHelper::getParam("categorie");
+            $sql = "select id_article, code_article, designation 
+                from articles
+                where categorie = ?
+                order by code_article";
+            $searchStmt = $pdo->prepare($sql);
+            $searchStmt->execute([$codeCategorie]);
+            $view = new View("/views/all_articles");
+            $view->setVar('searchStmt',$searchStmt);
+            $view->setVar('categorie', $designationCategorie);
+            return $view;
+        } catch(PDOException $exception) {
+            throw new PDOException($exception->getMessage(), (int)$exception->getCode());
+        }
     }
 
 }
